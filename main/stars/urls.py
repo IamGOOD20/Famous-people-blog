693ea85d57c3c48c4acd408ccb3a3bae9e96ac72
IamGOOD <rukshenas1990@gmail.com>
2022-08-02 14:33:13 +0300

    Third: working with basic tempaltes

--- main/stars/urls.py
+++ main/stars/urls.py
@@ -3,6 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('news/<int:newsid>/', news),
-    re_path(r'archive/(?P<year>[0-9]{4})/', archive),
+    path('about/', about, name='about'),
 ]
--- main/stars/views.py
+++ main/stars/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render, redirect
 
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 
+from .models import *
+
+site_map = ['About site', 'Add article', 'Feedback', 'Input']
+
 def index(request):
-      return HttpResponse('Stars page')
+      posts = Stars.objects.all()
+      return render(request, 'stars/index.html', {'posts': posts, 'site_map': site_map,  'title': 'Main page'})
+
+def about(request):
+      return render(request, 'stars/about.html', {'title': 'About site'})
 
 def news(request, newsid):
       return HttpResponse(f'<h1>There will be all news!<p>{newsid}</p></h1>')
