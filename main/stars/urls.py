6162f40bb51f52b7fac65acdc90c3c393e26149a
IamGOOD <rukshenas1990@gmail.com>
2022-08-06 18:20:16 +0300

    fourth: add 'Category' models

--- main/stars/urls.py
+++ main/stars/urls.py
@@ -4,4 +4,9 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
+    path('add article/', add_article, name='add_article'),
+    path('feedback/', feedback, name='feedback'),
+    path('login/', sign_in, name='sign_in'),
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('category/<int:cat_id>/', show_category, name='category'),
 ]
--- main/stars/views.py
+++ main/stars/views.py
@@ -4,24 +4,59 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 from .models import *
 
-site_map = ['About site', 'Add article', 'Feedback', 'Input']
+site_map = [
+      {'title': 'About site', 'url_name': 'about'},
+      {'title': 'Add article', 'url_name': 'add_article'},
+      {'title': 'Feedback', 'url_name': 'feedback'},
+      {'title': 'Sign in', 'url_name': 'sign_in'}
+      ]
 
 def index(request):
       posts = Stars.objects.all()
-      return render(request, 'stars/index.html', {'posts': posts, 'site_map': site_map,  'title': 'Main page'})
+      cats = Category.objects.all()
+
+      context = {
+            'posts': posts,
+            'cats': cats,
+            'site_map': site_map,
+            'title': 'Main page',
+            'cat_selected': 0,
+            }
+
+      return render(request, 'stars/index.html', context=context)
 
 def about(request):
       return render(request, 'stars/about.html', {'title': 'About site'})
 
-def news(request, newsid):
-      return HttpResponse(f'<h1>There will be all news!<p>{newsid}</p></h1>')
+def add_article(request):
+      return HttpResponse('Add article')
 
-def archive(request, year):
-      if int(year) > 2020:
-            return redirect('home', permanent=False)
+def feedback(request):
+      return HttpResponse('Feedback')
 
-      return HttpResponse(f'<h1>Archive by years</h1><p>{year}</p>')
+def sign_in(request):
+      return HttpResponse('Sign in')
+
+def show_post(request, post_id):
+      return HttpResponse(f'Article display with id = {post_id}')
+
+def show_category(request, cat_id):
+      posts = Stars.objects.filter(cat_id=cat_id)
+      cats = Category.objects.all()
+
+      context = {
+            'posts': posts,
+            'cats': cats,
+            'site_map': site_map,
+            'title': 'By category',
+            'cat_selected': cat_id,
+            }
+
+      if len(posts) == 0:
+            raise Http404
+
+      return render(request, 'stars/index.html', context=context)
+
 
 def pageNotFound(request, exception):
       return HttpResponseNotFound('<h1>Page not found</h1>')
-
