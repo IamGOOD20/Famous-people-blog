9ce4a591c6e16cabe9cf725a9db4530452a84dea
IamGOOD <rukshenas1990@gmail.com>
2022-08-07 23:41:41 +0300

    Fourth: add 'Category' models

--- main/stars/models.py
+++ main/stars/models.py
@@ -16,6 +16,7 @@ class Stars(models.Model):
       def get_absolute_url(self):
             return reverse('post', kwargs={'post_id': self.pk})
 
+
 class Category(models.Model):
       name = models.CharField(max_length=100, db_index=True)
 
